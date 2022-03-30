47a80e904d47c9cf6fdbe3ada984b4fa3090ada2
Cyril Dever <cdever@edgewhere.fr>
2022-03-30 10:46:28 +0200

    tree options in scala

---
+++ packages/scala/src/main/scala/com/cyrildever/merkle/tree/Options.scala
@@ -0,0 +1,16 @@
+package com.cyrildever.merkle.tree
+
+import com.cyrildever.hash.Hash.SHA_256
+
+/**
+ * Define the `MerkleTree` options
+ *
+ * @author  Cyril Dever
+ * @since   1.0
+ * @version 1.0
+ *
+ * @param doubleHash Set to `true` to hash nodes twice (default: `false`)
+ * @param engine The name of the hashing function (default: `"sha-256"`)
+ * @param sort Set to `true` to lexicographically sort leaves (default: `false`)
+ */
+case class Options(doubleHash: Boolean = false, engine: String = SHA_256, sort: Boolean = false)
